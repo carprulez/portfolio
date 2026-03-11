11f3bb82be02fbc03b3f6170986ec912f89ec2f2
carprulez <147204410+carprulez@users.noreply.github.com>
2026-03-11 10:18:05 -0700

    minor tweaks to formatting and comments

--- js/site.js
+++ js/site.js
@@ -1,20 +1,8 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
 // make sure document is ready
 $(document).ready(function() {
   console.log("Document ready.")
 
-  // Constants
-
   // Functions
-
-  // this is an example function and this comment tells what it doees and what parameters are passed to it.
-  function myFunction(param1, param2) {
-    // some code here
-    // return results;
-  }
 
   function main() {
     console.log("Main function started.");
